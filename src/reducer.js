b03cc7520ba2115c8dd97833b5174f1ca369a686
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-09-24 12:20:27 -0500

    cambios en reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,32 @@
-
+let idComentario = 0;
 export default function reducer(state = [], action){
-    
+    switch (action.type) {
+        case "AGREGAR":{
+            idComentario++
+            return [
+                ...state, 
+                {
+                    id:idComentario, 
+                    text:action.payload.text
+                }
+            ]
+        }
+        case "BORRAR":{
+            const index = state.findIndex(comentario => comentario.id === action.payload.id)
+            state.splice(index, 1)
+            return [
+                ...state
+            ]
+        }
+        case "ALTERNAR":{
+            const comentario = state.find(comentario => comentario.id === action.payload.id)
+            comentario.completado = !comentario.completado
+            return [
+                ...state
+            ]
+        }
+        default:{
+            return state
+        }
+    }
 }
