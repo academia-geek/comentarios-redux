15f1b056719d0f4baa14a31e8a0e9904a8fcd467
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-09-24 11:15:01 -0500

    construccion de redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Redux app");
+import {createStore} from './redux'
+
+function reducer(state = {},  action){
+    switch(action.type){
+        case 'CREAR' : {
+            return action.payload
+        }
+        case 'MODIFICAR' : {
+            return action.payload
+        }
+        default : {
+            return state
+        }
+    }
+}
+
+const store = createStore(reducer)
+
+store.subscribe(() => {
+    const state = store.getState()
+    console.log(state)
+})
+
+store.dispatch({
+    type : 'CREAR', 
+    payload : {
+        value : Math.random()
+    }
+})
