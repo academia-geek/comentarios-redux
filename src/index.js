814e1113a1f6c9196566c19014f7387bf9e0d002
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-09-24 12:08:15 -0500

    cambios en index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Practica de comentarios con redux")
+import store from './store'
+const listaComentariosDOM = document.querySelector("#listaComentarios")
+function actualizarListaComentarios(listaComentarios = []){
+    listaComentariosDOM.innerHTML = ''
+    listaComentarios.forEach(itemComentario => {
+        listaComentariosDOM.innerHTML +=  `
+                            <li class="itemComentario list-group-item">
+                                <input type="checkbox"  class="chkMarca form-check-input">
+                                <span>Hola mundo!!!</span>
+                                <button type="button" class="btn btn-outline-secundary pull-right">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </li>
+        `
+    })
+}
+
+store.subscribe(() => {
+    const state = store.getState()
+    actualizarListaComentarios(state)
+})
